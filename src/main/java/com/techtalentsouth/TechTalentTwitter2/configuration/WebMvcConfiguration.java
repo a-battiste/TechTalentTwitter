d3825502776b0f68a17e1dff0adbe4743ccf802f
Aris Battiste <aris.battiste@gmail.com>
2019-12-09 11:05:32 -0500

    Added controllers, security configuration and started login page

---
+++ src/main/java/com/techtalentsouth/TechTalentTwitter2/configuration/WebMvcConfiguration.java
@@ -0,0 +1,15 @@
+package com.techtalentsouth.TechTalentTwitter2.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfiguration implements WebMvcConfigurer {
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
+
+}
--- src/main/java/com/techtalentsouth/TechTalentTwitter2/service/UserService.java
+++ src/main/java/com/techtalentsouth/TechTalentTwitter2/service/UserService.java
@@ -1,9 +1,16 @@
 package com.techtalentsouth.TechTalentTwitter2.service;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.techtalentsouth.TechTalentTwitter2.model.Role;
+import com.techtalentsouth.TechTalentTwitter2.model.User;
 import com.techtalentsouth.TechTalentTwitter2.repository.RoleRepository;
 import com.techtalentsouth.TechTalentTwitter2.repository.UserRepository;
 
@@ -21,5 +28,32 @@ public class UserService {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+    
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+        
+    public List<User> findAll(){
+        return (List<User>) userRepository.findAll();
+    }
+        
+    public void save(User user) {
+        userRepository.save(user);
+    }
+    
+    public User saveNewUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(1);
+        Role userRole = roleRepository.findByRole("USER");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
+    
+    public User getLoggedInUser() {
+        String loggedInUsername = SecurityContextHolder.
+          getContext().getAuthentication().getName();
+        
+        return findByUsername(loggedInUsername);
     }
 }

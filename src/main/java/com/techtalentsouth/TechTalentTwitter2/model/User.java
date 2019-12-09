f5285a11adcefc261474559855e30ff6a4628d70
Aris Battiste <aris.battiste@gmail.com>
2019-12-09 09:08:06 -0500

    added User class to model package and css/js/SQL files

---
+++ src/main/java/com/techtalentsouth/TechTalentTwitter2/model/User.java
@@ -0,0 +1,17 @@
+package com.techtalentsouth.TechTalentTwitter2.model;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+}

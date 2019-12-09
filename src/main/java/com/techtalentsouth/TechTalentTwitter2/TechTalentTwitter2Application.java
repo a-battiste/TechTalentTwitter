7a448d8240caddd923c715bd582cceb79545c68c
Aris Battiste <aris.battiste@gmail.com>
2019-12-09 08:16:13 -0500

    Added dependencies

---
+++ src/main/java/com/techtalentsouth/TechTalentTwitter2/TechTalentTwitter2Application.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.TechTalentTwitter2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTalentTwitter2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTalentTwitter2Application.class, args);
+	}
+
+}

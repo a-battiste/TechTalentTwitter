c839554ed67846cb45202f6123ee5a5ddb6a8d9c
Aris Battiste <aris.battiste@gmail.com>
2019-12-10 21:55:05 -0500

    Added TweetRepository and TweetService

--- src/main/java/com/techtalentsouth/TechTalentTwitter2/repository/TweetRepository.java
+++ src/main/java/com/techtalentsouth/TechTalentTwitter2/repository/TweetRepository.java
@@ -1,5 +1,16 @@
 package com.techtalentsouth.TechTalentTwitter2.repository;
 
-public interface TweetRepository {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.techtalentsouth.TechTalentTwitter2.model.Tweet;
+import com.techtalentsouth.TechTalentTwitter2.model.User;
+
+@Repository
+public interface TweetRepository extends CrudRepository<Tweet, Long> {
+    List<Tweet> findAllByOrderByCreatedAtDesc();
+    List<Tweet> findAllByUserOrderByCreatedAtDesc(User user);
+    List<Tweet> findAllByUserInOrderByCreatedAtDesc(List<User> users);
 }

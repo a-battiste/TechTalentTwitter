a9b09d02f5179d4422a50376d385825d8a03eca4
Aris Battiste <aris.battiste@gmail.com>
2019-12-10 22:02:18 -0500

    Addded feed and newTweet GET methods to TweetController

--- src/main/java/com/techtalentsouth/TechTalentTwitter2/controller/TweetController.java
+++ src/main/java/com/techtalentsouth/TechTalentTwitter2/controller/TweetController.java
@@ -1,5 +1,35 @@
 package com.techtalentsouth.TechTalentTwitter2.controller;
 
-public class TweetController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.techtalentsouth.TechTalentTwitter2.model.Tweet;
+import com.techtalentsouth.TechTalentTwitter2.service.TweetService;
+import com.techtalentsouth.TechTalentTwitter2.service.UserService;
+
+@Controller
+public class TweetController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private TweetService tweetService;
+	
+	@GetMapping(value= {"/tweets", "/"})
+	public String getFeed(Model model){
+	    List<Tweet> tweets = tweetService.findAll();
+	    model.addAttribute("tweetList", tweets);
+	    return "feed";
+	}
+	
+	@GetMapping(value = "/tweets/new")
+	public String getTweetForm(Model model) {
+	    model.addAttribute("tweet", new Tweet());
+	    return "newTweet";
+	}
 }
